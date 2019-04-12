d5e547cf3498e587f574788f308197e15c2928c0
yoon-m <myoon1098@gmail.com>
2019-04-12 09:09:55 -0400

    render 3D globe, turned off rotation

---
+++ dist/main.js
@@ -0,0 +1,112 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ \"./src/test.js\");\n/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_test__WEBPACK_IMPORTED_MODULE_0__);\n\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n    // d3.json(\"src/data/draft_picks2017.json\")\n    //     .then(function(data) {\n    //         console.log(data);\n    //     }\n    // );\n\n    // let width = 860,\n    //     height = 660,\n    //     radius = 228,\n    //     mesh,\n    //     graticule,\n    //     scene = new THREE.Scene,\n    //     camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000),\n    //     renderer = new THREE.WebGLRenderer({ alpha: true });\n\n    // camera.position.z = 400;\n\n    // renderer.setPixelRatio(window.devicePixelRatio);\n    // renderer.setSize(width, height);\n\n    // document.body.appendChild(renderer.domElement);\n\n    // d3.json(\"https://unpkg.com/world-atlas@1/world/50m.json\", function (error, topology) {\n    //     if (error) throw error;\n    //     scene.add(graticule = wireframe(graticule10(), new THREE.LineBasicMaterial({ color: 0xaaaaaa })));\n    //     scene.add(mesh = wireframe(topojson.mesh(topology, topology.objects.land), new THREE.LineBasicMaterial({ color: 0xff0000 })));\n    //     d3.timer(function (t) {\n    //         graticule.rotation.x = mesh.rotation.x = Math.sin(t / 11000) * Math.PI / 3 - Math.PI / 2;\n    //         graticule.rotation.z = mesh.rotation.z = t / 10000;\n    //         renderer.render(scene, camera);\n    //     });\n    // });\n\n    // function vertex(point) {\n    //     let lambda = point[0] * Math.PI / 180,\n    //         phi = point[1] * Math.PI / 180,\n    //         cosPhi = Math.cos(phi);\n    //     return new THREE.Vector3(\n    //         radius * cosPhi * Math.cos(lambda),\n    //         radius * cosPhi * Math.sin(lambda),\n    //         radius * Math.sin(phi)\n    //     );\n    // }\n\n    // function wireframe(multilinestring, material) {\n    //     let geometry = new THREE.Geometry;\n    //     multilinestring.coordinates.forEach(function (line) {\n    //         d3.pairs(line.map(vertex), function (a, b) {\n    //             geometry.vertices.push(a, b);\n    //         });\n    //     });\n    //     return new THREE.LineSegments(geometry, material);\n    // }\n\n    // function graticule10() {\n    //     let epsilon = 1e-6,\n    //         x1 = 180, x0 = -x1, y1 = 80, y0 = -y1, dx = 10, dy = 10,\n    //         X1 = 180, X0 = -X1, Y1 = 90, Y0 = -Y1, DX = 90, DY = 360,\n    //         x = graticuleX(y0, y1, 2.5), y = graticuleY(x0, x1, 2.5),\n    //         X = graticuleX(Y0, Y1, 2.5), Y = graticuleY(X0, X1, 2.5);\n    //     function graticuleX(y0, y1, dy) {\n    //         let y = d3.range(y0, y1 - epsilon, dy).concat(y1);\n    //         return function (x) { return y.map(function (y) { return [x, y]; }); };\n    //     }\n    //     function graticuleY(x0, x1, dx) {\n    //         let x = d3.range(x0, x1 - epsilon, dx).concat(x1);\n    //         return function (y) { return x.map(function (x) { return [x, y]; }); };\n    //     }\n    //     return {\n    //         type: \"MultiLineString\",\n    //         coordinates: d3.range(Math.ceil(X0 / DX) * DX, X1, DX).map(X)\n    //             .concat(d3.range(Math.ceil(Y0 / DY) * DY, Y1, DY).map(Y))\n    //             .concat(d3.range(Math.ceil(x0 / dx) * dx, x1, dx).filter(function (x) { return Math.abs(x % DX) > epsilon; }).map(x))\n    //             .concat(d3.range(Math.ceil(y0 / dy) * dy, y1 + epsilon, dy).filter(function (y) { return Math.abs(y % DY) > epsilon; }).map(y))\n    //     };\n    // }\n\n    var context = d3.select('#content canvas')\n        .node()\n        .getContext('2d');\n\n    var projection = d3.geoAzimuthalEqualArea()\n        .scale(100)\n        .rotate([100, -10]);\n\n    var geoGenerator = d3.geoPath()\n        .projection(projection)\n        .context(context);\n\n    function update(geojson) {\n        context.lineWidth = 1;\n        context.strokeStyle = '#333';\n\n        context.beginPath();\n        geoGenerator({ type: 'FeatureCollection', features: geojson.features })\n        context.stroke();\n\n        // Graticule\n        var graticule = d3.geoGraticule();\n        context.beginPath();\n        context.strokeStyle = '#ccc';\n        geoGenerator(graticule());\n        context.stroke();\n\n        // London - New York\n        context.beginPath();\n        context.strokeStyle = 'red';\n        geoGenerator({ type: 'Feature', geometry: { type: 'LineString', coordinates: [[0.1278, 51.5074], [-74.0059, 40.7128]] } });\n        context.stroke();\n\n        // Circle\n        var circle = d3.geoCircle().center([0.1278, 51.5074]).radius(15)\n        context.beginPath();\n        context.strokeStyle = 'red';\n        geoGenerator(circle());\n        context.stroke();\n    }\n\n\n\n    // REQUEST DATA\n    d3.json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json', function (err, json) {\n        update(json)\n    })\n\n});\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/test.js":
+/*!*********************!*\
+  !*** ./src/test.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/test.js?");
+
+/***/ })
+
+/******/ });
---
+++ src/index.js
@@ -0,0 +1,129 @@
+import test from './test';
+
+document.addEventListener("DOMContentLoaded", () => {
+    // d3.json("src/data/draft_picks2017.json")
+    //     .then(function(data) {
+    //         console.log(data);
+    //     }
+    // );
+
+    // let width = 860,
+    //     height = 660,
+    //     radius = 228,
+    //     mesh,
+    //     graticule,
+    //     scene = new THREE.Scene,
+    //     camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000),
+    //     renderer = new THREE.WebGLRenderer({ alpha: true });
+
+    // camera.position.z = 400;
+
+    // renderer.setPixelRatio(window.devicePixelRatio);
+    // renderer.setSize(width, height);
+
+    // document.body.appendChild(renderer.domElement);
+
+    // d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function (error, topology) {
+    //     if (error) throw error;
+    //     scene.add(graticule = wireframe(graticule10(), new THREE.LineBasicMaterial({ color: 0xaaaaaa })));
+    //     scene.add(mesh = wireframe(topojson.mesh(topology, topology.objects.land), new THREE.LineBasicMaterial({ color: 0xff0000 })));
+    //     d3.timer(function (t) {
+    //         graticule.rotation.x = mesh.rotation.x = Math.sin(t / 11000) * Math.PI / 3 - Math.PI / 2;
+    //         graticule.rotation.z = mesh.rotation.z = t / 10000;
+    //         renderer.render(scene, camera);
+    //     });
+    // });
+
+    // function vertex(point) {
+    //     let lambda = point[0] * Math.PI / 180,
+    //         phi = point[1] * Math.PI / 180,
+    //         cosPhi = Math.cos(phi);
+    //     return new THREE.Vector3(
+    //         radius * cosPhi * Math.cos(lambda),
+    //         radius * cosPhi * Math.sin(lambda),
+    //         radius * Math.sin(phi)
+    //     );
+    // }
+
+    // function wireframe(multilinestring, material) {
+    //     let geometry = new THREE.Geometry;
+    //     multilinestring.coordinates.forEach(function (line) {
+    //         d3.pairs(line.map(vertex), function (a, b) {
+    //             geometry.vertices.push(a, b);
+    //         });
+    //     });
+    //     return new THREE.LineSegments(geometry, material);
+    // }
+
+    // function graticule10() {
+    //     let epsilon = 1e-6,
+    //         x1 = 180, x0 = -x1, y1 = 80, y0 = -y1, dx = 10, dy = 10,
+    //         X1 = 180, X0 = -X1, Y1 = 90, Y0 = -Y1, DX = 90, DY = 360,
+    //         x = graticuleX(y0, y1, 2.5), y = graticuleY(x0, x1, 2.5),
+    //         X = graticuleX(Y0, Y1, 2.5), Y = graticuleY(X0, X1, 2.5);
+    //     function graticuleX(y0, y1, dy) {
+    //         let y = d3.range(y0, y1 - epsilon, dy).concat(y1);
+    //         return function (x) { return y.map(function (y) { return [x, y]; }); };
+    //     }
+    //     function graticuleY(x0, x1, dx) {
+    //         let x = d3.range(x0, x1 - epsilon, dx).concat(x1);
+    //         return function (y) { return x.map(function (x) { return [x, y]; }); };
+    //     }
+    //     return {
+    //         type: "MultiLineString",
+    //         coordinates: d3.range(Math.ceil(X0 / DX) * DX, X1, DX).map(X)
+    //             .concat(d3.range(Math.ceil(Y0 / DY) * DY, Y1, DY).map(Y))
+    //             .concat(d3.range(Math.ceil(x0 / dx) * dx, x1, dx).filter(function (x) { return Math.abs(x % DX) > epsilon; }).map(x))
+    //             .concat(d3.range(Math.ceil(y0 / dy) * dy, y1 + epsilon, dy).filter(function (y) { return Math.abs(y % DY) > epsilon; }).map(y))
+    //     };
+    // }
+
+    var context = d3.select('#content canvas')
+        .node()
+        .getContext('2d');
+
+    var projection = d3.geoAzimuthalEqualArea()
+        .scale(100)
+        .rotate([100, -10]);
+
+    var geoGenerator = d3.geoPath()
+        .projection(projection)
+        .context(context);
+
+    function update(geojson) {
+        context.lineWidth = 1;
+        context.strokeStyle = '#333';
+
+        context.beginPath();
+        geoGenerator({ type: 'FeatureCollection', features: geojson.features })
+        context.stroke();
+
+        // Graticule
+        var graticule = d3.geoGraticule();
+        context.beginPath();
+        context.strokeStyle = '#ccc';
+        geoGenerator(graticule());
+        context.stroke();
+
+        // London - New York
+        context.beginPath();
+        context.strokeStyle = 'red';
+        geoGenerator({ type: 'Feature', geometry: { type: 'LineString', coordinates: [[0.1278, 51.5074], [-74.0059, 40.7128]] } });
+        context.stroke();
+
+        // Circle
+        var circle = d3.geoCircle().center([0.1278, 51.5074]).radius(15)
+        context.beginPath();
+        context.strokeStyle = 'red';
+        geoGenerator(circle());
+        context.stroke();
+    }
+
+
+
+    // REQUEST DATA
+    d3.json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json', function (err, json) {
+        update(json)
+    })
+
+});
