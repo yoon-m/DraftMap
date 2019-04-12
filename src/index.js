8ab5a8d8070eeb14bfcd0d97c23c2882642f0fd6
yoon-m <myoon1098@gmail.com>
2019-04-12 10:03:19 -0400

    spinning globe with circles that update location

--- src/index.js
+++ src/index.js
@@ -78,44 +78,114 @@ document.addEventListener("DOMContentLoaded", () => {
     //     };
     // }
 
-    var context = d3.select('#content canvas')
+
+
+
+
+    // var context = d3.select('#content canvas')
+    //     .node()
+    //     .getContext('2d');
+
+    // var projection = d3.geoOrthographic()
+    //     .scale(200)
+    //     .rotate([0, 0]);
+
+    // var geoGenerator = d3.geoPath()
+    //     .projection(projection)
+    //     .context(context);
+
+    // function update(geojson) {
+    //     context.lineWidth = 1;
+    //     context.strokeStyle = '#333';
+
+    //     context.beginPath();
+    //     geoGenerator({ type: 'FeatureCollection', features: geojson.features })
+    //     context.stroke();
+
+    //     // Graticule
+    //     var graticule = d3.geoGraticule();
+    //     context.beginPath();
+    //     context.strokeStyle = '#ccc';
+    //     geoGenerator(graticule());
+    //     context.stroke();
+
+    //     // London - New York
+    //     context.beginPath();
+    //     context.strokeStyle = 'red';
+    //     geoGenerator({ type: 'Feature', geometry: { type: 'LineString', coordinates: [[0.1278, 51.5074], [-74.0059, 40.7128]] } });
+    //     context.stroke();
+
+    //     // Circle
+    //     var circle = d3.geoCircle().center([0.1278, 51.5074]).radius(15)
+    //     context.beginPath();
+    //     context.strokeStyle = 'red';
+    //     geoGenerator(circle());
+    //     context.stroke();
+    // }
+
+
+
+    // // REQUEST DATA
+    // d3.json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json', function (err, json) {
+    //     update(json)
+    // })
+
+
+
+
+
+
+
+    let geojson = {}
+
+    let context = d3.select('#content canvas')
         .node()
         .getContext('2d');
 
-    var projection = d3.geoAzimuthalEqualArea()
-        .scale(100)
-        .rotate([100, -10]);
+    let projection = d3.geoOrthographic()
+        .scale(250);
 
-    var geoGenerator = d3.geoPath()
+    let geoGenerator = d3.geoPath()
         .projection(projection)
+        .pointRadius(4)
         .context(context);
 
-    function update(geojson) {
-        context.lineWidth = 1;
-        context.strokeStyle = '#333';
+    let yaw = 90;
+
+    function update() {
+        projection.rotate([yaw, -25])
+
+        context.clearRect(0, 0, 800, 600);
+
+        context.lineWidth = 0.5;
+        context.strokeStyle = 'black';
 
         context.beginPath();
         geoGenerator({ type: 'FeatureCollection', features: geojson.features })
         context.stroke();
 
+
         // Graticule
-        var graticule = d3.geoGraticule();
+        let graticule = d3.geoGraticule();
         context.beginPath();
-        context.strokeStyle = '#ccc';
+        context.strokeStyle = '#E0E0E0';
         geoGenerator(graticule());
         context.stroke();
 
-        // London - New York
+        yaw -= 0.5
+
+
+        // Circles
+        let circle1 = d3.geoCircle().center([0.1278, 51.5074]).radius(15)
         context.beginPath();
         context.strokeStyle = 'red';
-        geoGenerator({ type: 'Feature', geometry: { type: 'LineString', coordinates: [[0.1278, 51.5074], [-74.0059, 40.7128]] } });
+        geoGenerator(circle1());
         context.stroke();
 
-        // Circle
-        var circle = d3.geoCircle().center([0.1278, 51.5074]).radius(15)
+        let circle2 = d3.geoCircle().center([0.8, 5.5074]).radius(15)
         context.beginPath();
         context.strokeStyle = 'red';
-        geoGenerator(circle());
+        geoGenerator(circle2());
         context.stroke();
     }
 
@@ -123,7 +193,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // REQUEST DATA
     d3.json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json', function (err, json) {
-        update(json)
-    })
+        geojson = json;
+        window.setInterval(update, 30);
+    });
 
 });
